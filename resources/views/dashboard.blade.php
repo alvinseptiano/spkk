b62f862aaa25015b9df3feb89fc4387d05f8093b
alvinseptiano <spazmantiz.dev@gmail.com>
2024-12-27 21:55:48 +0700

    add ability to see result as long as they're signed

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,22 +1,17 @@
 <x-app-layout>
-    <section class="relative">
-        <div class="w-full shadow-md">
-            <div>
-                <!-- Image Background -->
-                <img class="rounded-lg object-cover w-full h-auto" src="{{url('/img/bg-dashboard.png')}}"
-                    alt="Dashboard Background">
+ <section class="relative">
+   <div class="w-full shadow-md">
+     <div class="relative">
+       <img class="rounded-lg object-cover w-full h-auto" src="{{url('/img/bg-dashboard.png')}}" alt="Dashboard Background">
+       
+       <h1 class="absolute inset-0 flex items-center justify-center text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4 text-center px-4 md:px-28 text-white">
+         SISTEM PENILAIAN KINERJA KARYAWAN (SPKK) DENGAN MENGGUNAKAN METODE SIMPLE ADDITIVE WEIGHTING DI PONDOK INDAH GOLF APARTMENT
+       </h1>
 
-                <h1
-                    class="absolute inset-0 flex items-center justify-center text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4 text-center px-4 md:px-28 text-white">
-                    SISTEM PENILAIAN KINERJA KARYAWAN (SPKK) DENGAN MENGGUNAKAN METODE SIMPLE ADDITIVE WEIGHTING DI
-                    PONDOK INDAH GOLF APARTMENT
-                </h1>
-            </div>
-
-            <div class="max-w-3xl mx-auto">
-                <div class="max-w-xs mx-auto sm:mx-auto sm:max-w-none sm:flex sm:justify-center">
-                </div>
-            </div>
-        </div>
-    </section>
+       <div class="absolute bottom-8 w-full flex justify-center z-10">
+         <a href="/hasilakhir" class="btn btn-primary text-white">Top Karyawan</a>
+       </div>
+     </div>
+   </div>
+ </section>
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -25,10 +25,10 @@ Route::middleware(['role:admin,manager'])->group(function () {
     Route::get('/listkaryawansearch', [KaryawanController::class, 'search'])->name('listkaryawansearch');
     Route::get('/nilaikaryawansearch', [KaryawanController::class, 'search'])->name('nilaikaryawansearch');
     Route::get('/nilaikaryawan', [KaryawanController::class, 'index'])->name('nilaikaryawan.index');
-    Route::get('/hasilakhir', [HasilController::class, 'index'])->name('hasil.index');
 });
 
 Route::middleware('auth')->group(function () {
+    Route::get('/hasilakhir', [HasilController::class, 'index'])->name('hasil.index');
     Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
